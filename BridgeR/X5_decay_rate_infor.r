6d81a64955066fef82fb2ee5d3a59e8ceee27e12
Naoto-Imamachi <imamachigatugatu@hotmail.co.jp>
2015-10-11 18:05:32 +0900

    no message

--- BridgeR/X5_decay_rate_infor.r
+++ BridgeR/X5_decay_rate_infor.r
@@ -12,7 +12,7 @@ group <- c("siCTRL","siStealth")
 files <- "C:/Users/Naoto/Documents/github/BRIC-seq_data_analysis/BridgeR/data/BridgeR_4_half-life_calculation.txt"
 
 ###Decay_rate_Infor_function###
-BridgeRHalfLifeComparison <- function(filename = "BridgeR_4_half-life_calculation.txt", group, hour, ComparisonFile, InforColumn = 4, OutputFig = "BridgeR_5_Half-life_comparison"){
+BridgeRHalfLifeComparison <- function(filename = "BridgeR_4_half-life_calculation.txt", group, hour, ComparisonFile, InforColumn = 4, LogScale=F, OutputFig = "BridgeR_5_Half-life_comparison"){
     #ComparisonFile: The length of vector must be 2 => c("Control","Knockdown")
     ###Import_library###
     library(data.table)
@@ -47,22 +47,29 @@ BridgeRHalfLifeComparison <- function(filename = "BridgeR_4_half-life_calculatio
         half_1_fig <- append(half_1_fig, half_1[x])
         half_2_fig <- append(half_2_fig, half_2[x])
         div <- half_1[x]/half_2[x]
-        if(div <= 0.5 || div >= 2){
+        if(div <= 0.5){
+            factor_fig <- append(factor_fig, 2)
+        }else if(div >= 2){
             factor_fig <- append(factor_fig, 1)
         }else{
             factor_fig <- append(factor_fig, 0)
         }
     }
-    half_1_fig <- log2(half_1_fig)
-    half_2_fig <- log2(half_2_fig)
-    counter <- length(which(factor_fig == 1))
-
+    if(LogScale){
+        half_1_fig <- log2(half_1_fig)
+        half_2_fig <- log2(half_2_fig)
+    }
+    up_genes <- length(which(factor_fig == 1))
+    down_genes <- length(which(factor_fig == 2))
+    
     plot_data <- data.frame(half_1_fig,half_2_fig,factor_fig)
     print_out <- paste("Plotted: ",length(plot_data[,1])," genes", sep="")
-    print_out2 <- paste("At least 2-fold: ",counter," genes", sep="")
+    print_out2 <- paste("At least 2-fold upregulated: ",up_genes," genes", sep="")
+    print_out3 <- paste("At least 2-fold downregulated: ",down_genes," genes", sep="")
     print(print_out)
     print(print_out2)
-
+    print(print_out3)
+    
     p.scatter <- ggplot()
     p.scatter <- p.scatter + layer(data=plot_data, 
                                    mapping=aes(x=half_1_fig, y=half_2_fig,colour=factor(factor_fig)), 
@@ -77,11 +84,70 @@ BridgeRHalfLifeComparison <- function(filename = "BridgeR_4_half-life_calculatio
     p.scatter <- p.scatter + xlab(name_xlab)
     p.scatter <- p.scatter + ylab(name_ylab)
     p.scatter <- p.scatter + theme(legend.position="none") #Remove Guides
-    p.scatter <- p.scatter + scale_colour_manual(values=c("black","red")) #Change color
+    p.scatter <- p.scatter + scale_colour_manual(values=c("black","red","blue")) #Change color
     plot(p.scatter)
         
     dev.off() #close_fig
     plot.new()
 }
+
+BridgeRHalfLifeDistribution <- function(filename = "BridgeR_4_half-life_calculation.txt", group, hour, ComparisonFile, InforColumn = 4, OutputFig = "BridgeR_5_Half-life_distribution"){
+    ###Import_library###
+    library(reshape2)
+    library(data.table)
+    library(ggplot2)
     
+    ###Prepare_file_infor###
+    time_points <- length(hour)
+    group_number <- length(group)
+    sample_number <- length(ComparisonFile)
+    comp_file_number <- NULL
+    figfile <- OutputFig
+    for(a in 1:sample_number){
+        comp_file_number <- append(comp_file_number, which(group == ComparisonFile[a]))
+        figfile <- paste(figfile,"_",ComparisonFile[a], sep="")
+    }
+    figfile <- paste(figfile,".png", sep="")
+    
+    input_file <- fread(filename, header=T)
+    png(filename=figfile,width = 1200, height = 1200)
+    
+    ###Plot_Half-life_distribution###
+    half_life_fig <- NULL
+    for(x in 1:sample_number){
+    #for(x in 1){
+        data_file <- NULL
+        half_life_column <- comp_file_number[x]*(time_points + InforColumn + 8)
+        half_life_data <- input_file[[half_life_column]]
+        half_life_data <- half_life_data[!is.na(half_life_data)]
+        if(x == 1){
+            Sample <-  as.factor(rep(ComparisonFile[x], length(half_life_data)))
+            half_life_fig <- data.frame(half_life_data, Sample)
+        }else{
+            Sample <-  as.factor(rep(ComparisonFile[x], length(half_life_data)))
+            half_life_fig <- rbind(half_life_fig, data.frame(half_life_data, Sample))
+        }
+    }
+
+    p.scatter <- ggplot()
+    p.scatter <- p.scatter + layer(data=half_life_fig, 
+                                   mapping=aes(x=half_life_data, colour=Sample), 
+                                   geom="freqpoly",
+                                   binwidth=0.1,
+                                   position = "identity",
+                                   size=1.2,
+                                   alpha=0.5)
+    p.scatter <- p.scatter + xlim(0,25)
+    p.scatter <- p.scatter + ggtitle("Half-life distribution")
+    p.scatter <- p.scatter + xlab("half-life")
+    p.scatter <- p.scatter + ylab("Fraction of transcripts(number)")
+    #library(scales)
+    #p.scatter <- p.scatter + scale_y_continuous(labels = percent)
+    plot(p.scatter)
+    
+    dev.off() #close_fig
+    plot.new()
+}
+
 BridgeRHalfLifeComparison(group=group, hour=hour, ComparisonFile=group)
+BridgeRHalfLifeDistribution(group=group, hour=hour, ComparisonFile=group)
